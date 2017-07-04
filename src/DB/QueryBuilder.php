9ec8a4309aa0f20322f6f9c6567eadf47c3d8d7d
undefcat <undefcat@gmail.com>
2017-07-04 17:23:32 +0900

    INSERT, DELETE, UPDATE 추가

--- src/DB/QueryBuilder.php
+++ src/DB/QueryBuilder.php
@@ -7,12 +7,21 @@ use Pofol\Support\Str;
 
 class QueryBuilder
 {
+    const STMT_SELECT = 0;
+    const STMT_INSERT = 1;
+    const STMT_DELETE = 2;
+    const STMT_UPDATE = 3;
+    const OPER_AND = 10;
+    const OPER_OR = 11;
+
     protected $pdo;
-    protected $method;
-    protected $columns;
+    protected $statement;
     protected $table;
+    protected $columns = [];
     protected $conditions = [];
     protected $whereQueue = [];
+    protected $values = [];
+    protected $boundValues = [];
 
     public function __construct(PDO $pdo, $table)
     {
@@ -22,7 +31,11 @@ class QueryBuilder
 
     public function select(...$columns)
     {
-        $this->method = 'SELECT';
+        if (isset($this->statement)) {
+            throw new Exception("이미 statement가 선언되었습니다.");
+        }
+
+        $this->statement = self::STMT_SELECT;
 
         if (is_array($columns[0])) {
             $columns = $columns[0];
@@ -33,9 +46,36 @@ class QueryBuilder
         return $this;
     }
 
+    public function insert(array $data)
+    {
+        if (isset($this->statement)) {
+            throw new Exception("이미 statement가 선언되었습니다.");
+        }
+
+        $this->statement = self::STMT_INSERT;
+
+        // $data가 2차원 배열이면 $data[0]이 존재할 것이다.
+        // 그리고 2차원 배열이란 뜻은 데이터를 여러개 넣었다는 뜻이다.
+        if (isset($data[0])) {
+            // 첫번째 배열엔 column 이름이 들어있다.
+            $this->columns = $data[0];
+
+            for ($i = 1, $length = count($data); $i < $length; $i++) {
+                $this->values[] = $data[$i];
+            }
+        } else {
+            foreach ($data as $key => $value) {
+                $this->columns[] = $key;
+                $this->values[] = $value;
+            }
+        }
+
+        $this->execute();
+    }
+
     public function where(...$conditions)
     {
-        $this->handlingWhereQueue('AND');
+        $this->handlingWhereQueue(self::OPER_AND);
         $this->handlingWhere($conditions);
 
         return $this;
@@ -43,10 +83,42 @@ class QueryBuilder
 
     public function orWhere(...$conditions)
     {
-        $this->handlingWhereQueue('OR');
+        $this->handlingWhereQueue(self::OPER_OR);
         $this->handlingWhere($conditions);
 
         return $this;
+    }
+
+    public function delete()
+    {
+        if (isset($this->statement)) {
+            throw new Exception("이미 statement가 선언되었습니다.");
+        }
+
+        $this->statement = self::STMT_DELETE;
+
+        $stmt = $this->execute();
+
+    }
+
+    public function update(array $data)
+    {
+        if (isset($this->statement)) {
+            throw new Exception("이미 statement가 선언되었습니다.");
+        }
+
+        $this->statement = self::STMT_UPDATE;
+
+        $temp = [];
+
+        foreach ($data as $key => $value) {
+            $this->values[] = "$key = ?";
+            $temp[] = $value;
+        }
+
+        $this->boundValues = array_merge($temp, $this->boundValues);
+
+        $stmt = $this->execute();
     }
 
     protected function handlingWhere($conditions)
@@ -78,10 +150,14 @@ class QueryBuilder
             return;
         }
 
-        if ($logicOperator === 'OR') {
-            $this->whereQueue[] = 'OR';
-        } else {
-            $this->whereQueue[] = 'AND';
+        switch ($logicOperator) {
+            case self::OPER_AND:
+                $this->whereQueue[] = 'AND';
+                return;
+
+            case self::OPER_OR:
+                $this->whereQueue[] = 'OR';
+                return;
         }
     }
 
@@ -90,10 +166,13 @@ class QueryBuilder
         $length = count($condition);
 
         if ($length === 2) {
-            $condition = [$condition[0], '=', $this->revealedType($condition[1])];
+            $this->boundValues[] = $condition[1];
+            $condition = ["`{$condition[0]}`", '=', '?'];
             return $condition;
         } elseif ($length === 3) {
-            $condition[2] = $this->revealedType($condition[2]);
+            $this->boundValues[] = $condition[2];
+            $condition[0] = "`{$condition[0]}`";
+            $condition[2] = '?';
             return $condition;
         } else {
             throw new Exception("where 형식이 잘못되었습니다.");
@@ -107,17 +186,7 @@ class QueryBuilder
 
     public function get()
     {
-        if (!isset($this->method)) {
-            throw new Exception("쿼리 방식이 선언되지 않았습니다.");
-        }
-
-        $query = $this->buildQuery();
-
-        var_dump($query);
-
-        $stmt = $this->pdo->prepare($query);
-
-        $stmt->execute();
+        $stmt = $this->execute();
 
         $tableName = Str::toCamel($this->table);
         $filePath = __PF_ROOT__ . '\\' . 'app\\' . $tableName . '.php';
@@ -130,20 +199,58 @@ class QueryBuilder
         return $stmt->fetchAll(PDO::FETCH_OBJ);
     }
 
+    protected function execute()
+    {
+        if (!isset($this->statement)) {
+            throw new Exception("쿼리 statement가 선언되지 않았습니다.");
+        }
+
+        $query = $this->buildQuery();
+
+        var_dump($query);
+        var_dump($this->boundValues);
+
+        $stmt = $this->pdo->prepare($query);
+
+        for ($i = 0, $len = count($this->boundValues); $i < $len; $i++) {
+            $value = $this->boundValues[$i];
+
+            $stmt->bindValue($i + 1, $value, $this->revealedType($value));
+        }
+
+        $stmt->execute();
+
+        return $stmt;
+    }
+
     protected function buildQuery()
     {
         $query = [];
 
-        switch ($this->method) {
-            case 'SELECT':
+        switch ($this->statement) {
+            case self::STMT_SELECT:
                 $this->buildSelect($query);
                 $this->buildFrom($query);
+                $this->buildWhereIfExist($query);
+                break;
 
-                if (empty($this->conditions)) {
-                    break;
-                }
+            case self::STMT_INSERT:
+                $this->buildInsert($query);
+                $this->buildValues($query);
+                break;
 
-                $this->buildWhere($query);
+            case self::STMT_DELETE:
+                $this->buildDelete($query);
+                $this->buildWhereIfExist($query);
+                break;
+
+            case self::STMT_UPDATE:
+                $this->buildUpdate($query);
+                $this->buildWhereIfExist($query);
+                break;
+
+            default:
+                throw new Exception("쿼리 statement가 선언되지 않았습니다.");
         }
 
         return join(' ', $query);
@@ -151,21 +258,28 @@ class QueryBuilder
 
     protected function buildSelect(&$query)
     {
-        $query[] = $this->method;
+        $query[] = 'SELECT';
+
+        $this->wrapGraves($this->columns);
+
         $query[] = join(', ', $this->columns);
     }
 
     protected function buildFrom(&$query)
     {
-        $query[] = 'FROM ' . $this->table;
+        $query[] = "FROM `{$this->table}`";
     }
 
-    protected function buildWhere(&$query)
+    protected function buildWhereIfExist(&$query)
     {
+        if (empty($this->conditions)) {
+            return;
+        }
+        // stack을 뒤집어서 queue로 사용한다.
         if (count($this->whereQueue) > 1) {
             $this->whereQueue = array_reverse($this->whereQueue);
         }
-
+        // dummy값 제거
         array_pop($this->whereQueue);
 
         $where = [];
@@ -181,27 +295,79 @@ class QueryBuilder
         $query[] = 'WHERE ' . join(' ', $where);
     }
 
+    protected function buildInsert(&$query)
+    {
+        $query[] = "INSERT INTO `{$this->table}`";
+
+        $this->wrapGraves($this->columns);
+
+        $query[] = '(' . join(', ', $this->columns) . ')';
+    }
+
+    protected function buildDelete(&$query)
+    {
+        $query[] = "DELETE FROM `{$this->table}`";
+    }
+
+    protected function buildUpdate(&$query)
+    {
+        $query[] = "UPDATE `{$this->table}` SET";
+
+        $this->wrapGraves($this->values);
+
+        $query[] = join(', ', $this->values);
+    }
+
+
+    protected function buildValues(&$query)
+    {
+        $query[] = 'VALUES';
+        $values = [];
+
+        if (!is_array($this->values[0])) {
+            for ($i = 0, $len = count($this->values); $i < $len; $i++) {
+                $this->boundValues[] = $this->values[$i];
+                $values[] = '?';
+            }
+            $query[] = '(' . join(', ', $values) . ')';
+        } else {
+            foreach ($this->values as $value) {
+                for ($i = 0, $len = count($value); $i < $len; $i++) {
+                    $this->boundValues[] = $value[$i];
+                    $value[$i] = '?';
+                }
+
+                $values[] = '(' . join(', ', $value) . ')';
+            }
+
+            $query[] = join(', ', $values);
+        }
+    }
+
+    protected function wrapGraves(array &$values)
+    {
+        array_walk($values, function (&$item) {
+            $item = "`$item`";
+        });
+    }
+
     protected function revealedType($value)
     {
         switch (gettype($value)) {
             case 'string':
-                return "'{$value}'";
+                return PDO::PARAM_STR;
 
             case 'integer':
-                return $value;
+                return PDO::PARAM_INT;
 
             case 'NULL':
-                return 'NULL';
+                return PDO::PARAM_NULL;
 
             case 'boolean':
-                if ($value) {
-                    return 'TRUE';
-                } else {
-                    return 'FALSE';
-                }
+                return PDO::PARAM_BOOL;
 
             default:
-                return $value;
+                return PDO::PARAM_STR;
         }
     }
 }
