23c0078d69daae56b79ea6208d60a3462d7c1861
undefcat <undefcat@gmail.com>
2017-07-13 20:20:54 +0900

    randString 추가

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -82,3 +82,18 @@ if (!function_exists('__viewExtends')) {
         return ViewExtends::instance();
     }
 }
+
+if (!function_exists('randString')) {
+
+    function randString($len = 50)
+    {
+        $string = "";
+        $chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for ($i = 0; $i < $len; $i++) {
+            $string .= substr($chars, rand(0, strlen($chars)), 1);
+        }
+
+        return $string;
+    }
+}
